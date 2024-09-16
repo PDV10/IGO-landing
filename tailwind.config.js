83d8d659ce57b1bc3bbbb15af987d2fcfbc7db50
pato <patooodv@gmail.com>
2024-09-16 12:08:15 -0300

    Se extiende colores en archivo tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "gris-claro": "#e8e8e8",
+        "gris-oscuro": "#878787",
+      },
+    },
   },
   plugins: [],
 };
