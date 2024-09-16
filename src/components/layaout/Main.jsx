9ab7e48d0e7e97cd547b03d888600c8ddfaa382e
pato <patooodv@gmail.com>
2024-09-16 17:08:12 -0300

    NEW: se agrega footer (primera version)

--- src/components/layaout/Main.jsx
+++ src/components/layaout/Main.jsx
@@ -2,7 +2,7 @@ import { Button } from "@material-tailwind/react";
 export default function Main() {
   return (
     <>
-      <Button>Button</Button>
+      <Button>button</Button>
     </>
   );
 }
