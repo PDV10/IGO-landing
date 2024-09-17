f781981c5e63cec1eb652e03348b7d3973e8ce1d
pato <patooodv@gmail.com>
2024-09-17 20:04:23 -0300

    FIX: se acomoda header flotante

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <section className="min-h-screen bg-gris-oscuro"></section>;
+  return (
+    <section className="min-h-screen bg-[url('./src/assets/chef.jpg')] bg-cover bg-center h-screen flex justify-center items-center">
+      <h1 className="text-4xl text-center font-bold text-white absolute top-[30%] uppercase break-words">
+        Descubre el arte culinario
+      </h1>
+      <p className="text-2xl text-center font-bold text-white absolute top-[40%] ">
+        hola mundo
+      </p>
+    </section>
+  );
 }
