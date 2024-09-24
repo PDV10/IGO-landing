254d88285d1e7302d12738a4f97f2c5f275030f2
carlossalvaje <tomasabelmaneri@gmail.com>
2024-09-24 20:22:16 -0300

    EDIT: rediseño de Home responsive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,24 +3,35 @@ export default function Home() {
   return (
     <Element
       name="Home"
-      className="relative min-h-screen bg-[url('./src/assets/IMG-HOME-PRINCIPAL.jpg')] bg-cover bg-center flex justify-center md:justify-start items-center"
+      className="relative min-h-screen bg-[url('./src/assets/IMG-HOME-PRINCIPAL-MOVILE.png')] bg-cover bg-center flex justify-center items-center md:bg-[url('./src/assets/IMG-HOME-PRINCIPAL.jpg')]"
     >
-      <div className="absolute bottom-0 left-0 w-full h-2/4 bg-gradient-to-t from-gray-400 to-transparent"></div>
+      <div className="absolute bottom-0 left-0 w-full h-3/4 bg-gradient-to-t from-gray-400 to-transparent"></div>
 
-      <div className="relative flex flex-col items-center md:items-start text-center md:text-left md:pl-[9rem]">
-        <h1 className="sm:py-10 text-6xl sm:text-5xl md:text-6xl font-bold text-black md:py-4 lg:text-[4.375rem]">
-          <span>Descubre el arte culinario</span>
-        </h1>
+      <div className="relative flex flex-col items-start w-full pl-1 msm:pl-10">
+        <div className="mb-44">
+          <h1 className="text-black font-bold sm:text-5xl md:text-8xl lg:pt-10 xl:pt-10 xl:pl-32 lg:text-9xl text-left uppercase">
+            <span className="block mb-2 hover:opacity-100 transition-opacity duration-500 opacity-50">
+              Descubre
+            </span>
+            <span className="block mb-2 hover:opacity-100 transition-opacity duration-500 opacity-50">
+              el arte
+            </span>
+            <span className="block hover:opacity-100 transition-opacity duration-500 opacity-50">
+              culinario
+            </span>
+          </h1>
+        </div>
 
-        <p className="text-2xl sm:text-xl md:text-3xl font-bold text-black whitespace-normal md:mt-10 lg:text-[1.75rem]">
-          <span>Donde la tradición</span>
-          <br />
-          <span>se fusiona con la innovación</span>
-        </p>
-
-        <button className="mt-36 bg-black text-white font-bold py-[0.625rem] px-4 text-lg md:text-[1.5rem] rounded-lg transition-all duration-300 hover:bg-gray-800 hover:scale-105">
-          Ver más
-        </button>
+        <div className="mx-auto -ml-1/2 -mr-1/2 translate-y-35 xl:translate-y-20 ">
+          <button className="bg-black text-white font-bold p-4 xl:p-6 xl:text-xl sm:translate-y-24 xl:translate-y-2 rounded-3xl transition-all duration-300 hover:bg-gray-800 hover:scale-105 flex items-center">
+            <p className="xl:text-5xl mr-4 uppercase">inscripciones 2025</p>
+            <img
+              src="../../src/assets/ICON-CHECK.png"
+              alt="icon-check"
+              className="xl:w-[60px] w-[40px] h-auto"
+            />
+          </button>
+        </div>
       </div>
     </Element>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,13 +13,19 @@ module.exports = withMT({
       colors: {
         "gris-claro": "#e8e8e8",
         "gris-oscuro": "#878787",
+        "custom-color": "FF5733",
       },
       screens: {
         sm: "320px",
+        msm: "640px",
         xl: "1280px",
+        xs: "400px",
       },
       boxShadow: {
         centrada: "2px 2px 2px rgba(0, 0, 0, 0.8)",
+      },
+      transitionProperty: {
+        "width-color": "width, color",
       },
     },
   },
