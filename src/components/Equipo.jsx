a18543c1ec30d9590cbd257de9f9cb793434dd5a
carlossalvaje <tomasabelmaneri@gmail.com>
2024-09-25 09:04:34 -0300

    EDIT: pequeños cambios en el layout de Equipo

--- src/components/Equipo.jsx
+++ src/components/Equipo.jsx
@@ -1,15 +1,15 @@
 import { Element } from "react-scroll";
 export default function Equipo() {
   return (
-    <Element name="Conocenos" className="w-full p-3 bg-gray-300">
+    <Element name="Conocenos" className="w-full p-3 bg-gray-400">
       <div className="min-h-[880px] overflow-hidden">
         <div className="flex flex-col md:flex-row gap-4 h-full">
           <div className="flex flex-col items-center justify-center rounded-md p-6 md:w-1/2">
-            <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-center uppercase">
+            <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-center uppercase xl:text-9xl xl:text-start">
               Nuestro equipo docente!
             </h1>
 
-            <p className="hidden md:block text-center text-xl md:text-2xl lg:text-3xl pt-20">
+            <p className="hidden md:block text-center text-xl md:text-2xl lg:text-3xl pt-20 xl:text-start xl:text-5xl">
               En IGO, contamos con un equipo docente de excelencia, integrado
               por profesionales de la gastronomía con amplia experiencia y una
               verdadera vocación. Cada uno de nuestros docentes se dedica a
@@ -18,9 +18,9 @@ export default function Equipo() {
             </p>
           </div>
 
-          <div className="flex flex-col md:flex-row gap-2 mb-8 h-full md:w-1/2">
-            <div className="grid grid-cols-2 gap-2 w-full md:p-[50px]">
-              <div className="row-span-3 flex items-center justify-center w-full h-auto rounded-md shadow-[0px_0px_6px_rgba(0,0,0,8)]">
+          <div className="flex flex-col md:flex-row gap-2 mb-8 h-auto md:w-1/2 xl:min-h-screen xl:p-28 xl:m-0">
+            <div className="grid grid-cols-2 gap-[10px] sm:gap-[20px] msm:gap-[25px] xl:gap-[30px] w-full md:p-[50px]">
+              <div className="row-span-3 flex items-center justify-center w-full h-auto rounded-md shadow-[0px_0px_6px_rgba(0,0,0,8)] mb-4">
                 <img
                   src="../../src/assets/profesores/IMG-PROFE (2).jpg"
                   alt="#"
@@ -28,7 +28,7 @@ export default function Equipo() {
                 />
               </div>
 
-              <div className="flex items-center justify-center rounded-md shadow-[0px_0px_6px_rgba(0,0,0,8)]">
+              <div className="flex items-center justify-center rounded-md shadow-[0px_0px_6px_rgba(0,0,0,8)] mb-4">
                 <img
                   src="../../src/assets/profesores/IMG-PROFE (1).jpg"
                   alt="#"
@@ -36,7 +36,7 @@ export default function Equipo() {
                 />
               </div>
 
-              <div className="flex items-center justify-center max-h-[100vh] rounded-md shadow-[0px_0px_6px_rgba(0,0,0,8)]">
+              <div className="flex items-center justify-center max-h-[100vh] rounded-md shadow-[0px_0px_6px_rgba(0,0,0,8)] mb-4">
                 <img
                   src="../../src/assets/profesores/IMG-PROFE (4).jpg"
                   alt="#"
